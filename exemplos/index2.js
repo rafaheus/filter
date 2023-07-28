9b649e10ba76c992bdc07127cf0f6e854da2547e
Rafael Gomes Heuseler <rafaelheuseler@hotmail.com>
2023-07-27 22:18:21 -0300

    first commit

---
+++ exemplos/index2.js
@@ -0,0 +1,7 @@
+const numeros = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3,]
+
+const resultado = numeros.filter((numero) => {
+    return numero === 2;
+});
+
+console.log(resultado);
---
+++ exemplos/index4.js
@@ -0,0 +1,7 @@
+const nomes = ['joao', 'maria','julia']
+
+const resultado = nomes.filter((nome) => {
+    return nome === 'julia';
+});
+
+console.log(resultado);
